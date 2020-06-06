ea08059374a3784bc9e4aad0b66850fa171f6255
Leo Borba <leonardo.s.borba@gmail.com>
2020-06-06 12:14:54 -0300

    implement days on the calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -6,10 +6,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
+  weekDays: string[];
+  currentDate: Date;
+  currentDay: Date;
+  daysOfMonth: Date[] = [];
 
-  constructor() { }
+  constructor() {
+    this.weekDays = [
+      'sanday',
+      'monday',
+      'tuesday',
+      'wednesday',
+      'thusday',
+      'friday',
+      'saturday',
+    ];
+
+    this.currentDate = new Date();
+    this.currentDay = new Date();
+    this.daysOfMonth = this.getDays(this.currentDate);
+    this.getDays(this.currentDate);
+  }
 
   ngOnInit() {
+  }
+  
+  getDays(currentDate: Date):Date[] {
+    const daysOnMounth = (new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)).getDate();
+    const daysOfMonth = Array(...(Array(daysOnMounth + 1).keys())).slice(1);
+    const days = daysOfMonth.map(day => {
+      return this.resetDateTime(new Date(currentDate.setDate(day)));
+    });
+
+    const daysBefore = this.handleDaysBeforeCurrentMonth(days[0])
+    const daysAfter = this.handleDaysAfterCurrentMonth(days[days.length - 1])
+    
+    return [...daysBefore, ...days, ...daysAfter]
+  }
+
+  handleDaysBeforeCurrentMonth(date: Date) {
+    const currentDate = new Date(date);
+    const daysBefore = [];
+    while (currentDate.getDay() > 0) {
+      currentDate.setDate(currentDate.getDate() - 1)
+      daysBefore.unshift(new Date(currentDate));
+    }
+
+    return daysBefore;
+  }
+
+
+  handleDaysAfterCurrentMonth(date: Date) {
+    const currentDate = new Date(date);
+    const daysAfter = [];
+    while (currentDate.getDay() < 6) {
+      currentDate.setDate(currentDate.getDate() + 1)
+      daysAfter.push(new Date(currentDate));
+    }
+
+    return daysAfter;
+  }
+
+  resetDateTime(date: Date) {
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+
+    return date
   }
 
 }
