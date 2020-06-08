7195c037045bd3005fdb60337200cc54121710b3
Leo Borba <leonardo.s.borba@gmail.com>
2020-06-08 17:17:27 -0300

    unit test max chars

--- src/app/event/event.component.spec.ts
+++ src/app/event/event.component.spec.ts
@@ -41,6 +41,12 @@ describe('EventComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should check title max 30 chars', () => {
+    component.form.controls.title.setValue('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum')
+    expect(component.save()).toEqual(null);
+    expect(component.showHint('title')).toEqual(true);
+  });
+
   it('should return the event data updated', () => {
     const dateFake = new Date();
     component.eventData = {
